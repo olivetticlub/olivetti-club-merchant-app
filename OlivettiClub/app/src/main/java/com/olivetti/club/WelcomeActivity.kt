c319ea654a34ddff78cc351b6a5349eff4101f01
XPeppers <nicola.margoni@xpeppers.com>
2019-10-05 16:30:26 +0200

    creates merchant on boarding page

--- OlivettiClub/app/src/main/java/com/olivetti/club/WelcomeActivity.kt
+++ OlivettiClub/app/src/main/java/com/olivetti/club/WelcomeActivity.kt
@@ -1,13 +1,20 @@
 package com.olivetti.club
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        nextButton.setOnClickListener {
+            startActivity(Intent(this, MerchantOnBoarding::class.java))
+            finish()
+        }
     }
 
 }
