171f07714de8db8ba6df8bc2418f202f6676f846
XPeppers <nicola.margoni@xpeppers.com>
2019-10-05 15:58:19 +0200

    creates welcome page

---
+++ OlivettiClub/app/src/main/java/com/olivetti/club/WelcomeActivity.kt
@@ -0,0 +1,13 @@
+package com.olivetti.club
+
+import android.app.Activity
+import android.os.Bundle
+
+class WelcomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+}
