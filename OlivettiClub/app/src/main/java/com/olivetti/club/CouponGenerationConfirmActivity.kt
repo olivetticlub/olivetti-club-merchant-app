f0a57dc472566285ba14acd298ff593f849dff3a
XPeppers <nicola.margoni@xpeppers.com>
2019-10-05 17:32:32 +0200

    creates Coupon generation confirm page

---
+++ OlivettiClub/app/src/main/java/com/olivetti/club/CouponGenerationConfirmActivity.kt
@@ -0,0 +1,12 @@
+package com.olivetti.club
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CouponGenerationConfirmActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_coupon_generation)
+    }
+}
